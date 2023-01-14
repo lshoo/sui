7f3c472bb19c49b9c049faad522011de5ad29e01
lshoo <lshoo@users.noreply.github.com>
2023-01-14 02:15:42 +0000

    deploy: 3acef2482fb5d9a9a3bb016b0806bf26057c5995

---
+++ sui_indexer/metrics/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":[["IndexerAddressProcessorMetrics","derivative data processor related metrics"],["IndexerEventHandlerMetrics",""],["IndexerObjectProcessorMetrics",""],["IndexerPackageProcessorMetrics",""],["IndexerTransactionHandlerMetrics","Prometheus metrics for sui-indexer."]]};
